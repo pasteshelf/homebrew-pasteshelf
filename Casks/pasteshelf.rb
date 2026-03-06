67b932004da5ab249040d25513c266f348b7b80b
harungungorer <harungungorer@users.noreply.github.com>
2026-03-06 22:33:54 +0300

    chore: bump pasteshelf to v0.1.0

--- Casks/pasteshelf.rb
+++ Casks/pasteshelf.rb
@@ -1,6 +1,6 @@
 cask "pasteshelf" do
-  version "__VERSION__"
-  sha256 "__SHA256__"
+  version "0.1.0"
+  sha256 "4113b67ce36243daac3416233cde92f89a6f4731ec2bbd74b478dfbdfee6e488"
 
   url "https://github.com/pasteshelf/PasteShelf/releases/download/v#{version}/PasteShelf-#{version}.dmg"
   name "PasteShelf"
