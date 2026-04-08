e5373b4c9535c270f345deec035772450f4bf615
harungungorer <harungungorer@users.noreply.github.com>
2026-04-08 22:28:05 +0300

    chore: bump pasteshelf to v1.0.3

--- Casks/pasteshelf.rb
+++ Casks/pasteshelf.rb
@@ -1,8 +1,8 @@
 cask "pasteshelf" do
-  version "1.0.2"
-  sha256 "05925f28a1acc19f6adf78c961263f3e230e565948e47184ae8c9c185c0eb982"
+  version "1.0.3"
+  sha256 "9492bdc00b3fa6ba70b33e7d628299e2eadfe3c8d35bfc7e2f3d816fc8140023"
 
-  url "https://github.com/pasteshelf/PasteShelf/releases/download/v#{version}/PasteShelf-#{version}.dmg"
+  url "https://github.com/pasteshelf/PasteShelf/releases/download/v#{version}/PasteShelf.dmg"
   name "PasteShelf"
   desc "Privacy-first clipboard manager for macOS"
   homepage "https://github.com/pasteshelf/PasteShelf"
