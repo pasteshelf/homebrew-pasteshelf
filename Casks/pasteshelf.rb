e1fb1e4f608af3ba82c7c28c492270513b5a36c8
harungungorer <harungungorer@users.noreply.github.com>
2026-04-08 22:05:45 +0300

    fix: correct DMG URL for v1.0.2 (still uses versioned name)

--- Casks/pasteshelf.rb
+++ Casks/pasteshelf.rb
@@ -2,7 +2,7 @@ cask "pasteshelf" do
   version "1.0.2"
   sha256 "05925f28a1acc19f6adf78c961263f3e230e565948e47184ae8c9c185c0eb982"
 
-  url "https://github.com/pasteshelf/PasteShelf/releases/download/v#{version}/PasteShelf.dmg"
+  url "https://github.com/pasteshelf/PasteShelf/releases/download/v#{version}/PasteShelf-#{version}.dmg"
   name "PasteShelf"
   desc "Privacy-first clipboard manager for macOS"
   homepage "https://github.com/pasteshelf/PasteShelf"
