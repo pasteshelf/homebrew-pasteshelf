1f303216cf5088efd55cbc47c0b2dbfc1407d8b8
harungungorer <harungungorer@users.noreply.github.com>
2026-06-17 23:03:03 +0300

    chore: bump pasteshelf to v1.0.22

--- Casks/pasteshelf.rb
+++ Casks/pasteshelf.rb
@@ -1,6 +1,6 @@
 cask "pasteshelf" do
-  version "1.0.21"
-  sha256 "b25c5ca8354a322a000af1ae0952ed84873a06fe76ec5d2f3c0ca1d19c31e982"
+  version "1.0.22"
+  sha256 "70298503f4f4cb6e12d193fa1022b2c18ae585a84f7de1cc6be9f18272fa71df"
 
   url "https://github.com/pasteshelf/PasteShelf/releases/download/v#{version}/PasteShelf.dmg"
   name "PasteShelf"
